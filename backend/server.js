41e1020b81b6302075a8a324ecf323b42d0eb5f4
peterKhanh <tkhn2020@gmail.com>
2025-03-28 17:11:10 +0700

    add Admin LTE 3

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,13 @@ const mysql = require("mysql");
 const cors = require("cors");
 
 const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.use(express.json())
 app.use(cors())
+
+
+
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -22,57 +27,61 @@ db.connect(function(err) {
     }
 });
 
-app.get("/", (req, res)=>{
+app.get('', function(req, res){
+    res.render('home');
+});
 
-    const sql = "SELECT * FROM person";
-    db.query(sql, (err, data)=>{
-        if (err) return res.json("Errors");
-        return res.json(data)    ;
-    })
-})
 
-app.post("/add", (req, res) =>{
-    const sql = "INSERT INTO person (`username`, `password`, `city`) VALUES (?)";
-    const values = [
-        req.body.username,
-        req.body.password,
-        req.body.city
-    ]
+// app.get("/", (req, res)=>{
+
+//     const sql = "SELECT * FROM person";
+//     db.query(sql, (err, data)=>{
+//         if (err) return res.json("Errors");
+//         return res.json(data)    ;
+//     })
+// })
+
+// app.post("/add", (req, res) =>{
+//     const sql = "INSERT INTO person (`username`, `password`, `city`) VALUES (?)";
+//     const values = [
+//         req.body.username,
+//         req.body.password,
+//         req.body.city
+//     ]
    
-    db.query(sql, [values], (err, data)=>{
-        if (err) return res.json("Errors");
-        return res.json(data)    ;
-    })
-})
+//     db.query(sql, [values], (err, data)=>{
+//         if (err) return res.json("Errors");
+//         return res.json(data)    ;
+//     })
+// })
 
 
-app.put('/update/:id', (req, res) =>{
-    const sql = "Update person Set `username` = ? , `password` = ?, `city` =? where pid = ?";
-    const values = [
-        req.body.username,
-        req.body.password,
-        req.body.city,
-    ]
-   const id = req.params.id
+// app.put('/update/:id', (req, res) =>{
+//     const sql = "Update person Set `username` = ? , `password` = ?, `city` =? where pid = ?";
+//     const values = [
+//         req.body.username,
+//         req.body.password,
+//         req.body.city,
+//     ]
+//    const id = req.params.id
 
-    db.query(sql, [...values, id], (err, data)=>{
-        if (err) return res.json("Errors");
-        return res.json(data)    ;
-    })
-})
+//     db.query(sql, [...values, id], (err, data)=>{
+//         if (err) return res.json("Errors");
+//         return res.json(data)    ;
+//     })
+// })
 
 
-app.delete('/delete/:id', (req, res) =>{
-    const sql = "Delete from person where pid = ?";
+// app.delete('/delete/:id', (req, res) =>{
+//     const sql = "Delete from person where pid = ?";
   
-   const id = req.params.id
+//    const id = req.params.id
    
-    db.query(sql, [id], (err, data)=>{
-        if (err) return res.json("Errors");
-        return res.json(data)    ;
-    })
-})
-
+//     db.query(sql, [id], (err, data)=>{
+//         if (err) return res.json("Errors");
+//         return res.json(data)    ;
+//     })
+// })
 
 app.listen(8081, ()=>{
     console.log("Listening: 8081");
